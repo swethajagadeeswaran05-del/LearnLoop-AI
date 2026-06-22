6422cc8d67d1e101ce5278b1b3983aa1efaf9ad3
swethajagadeeswaran05-del <swethajagadeeswaran05@gmail.com>
2026-06-22 21:16:19 +0530

    Delete src directory

--- src/main/java/com/learnloop/backend/repository/MentorAssessmentQuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.learnloop.backend.repository;
-
-import com.learnloop.backend.model.MentorAssessmentQuestion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MentorAssessmentQuestionRepository extends JpaRepository<MentorAssessmentQuestion, Long> {
-}
